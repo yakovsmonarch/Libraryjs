13776665e54a73f4ed38b2346af16bc39465faea
yakovsmonarch <frag.2010@yandex.ru>
2025-12-29 15:46:53 +0800

    Добавил смену фона.

--- hello.js
+++ hello.js
@@ -1,11 +1,30 @@
 class HelloWorld {
-    sayHello() {
+    #colors = [
+        '#FF6B6B', // Красный
+        '#4ECDC4', // Бирюзовый
+        '#45B7D1', // Голубой
+        '#96CEB4', // Зеленый
+        '#FFEAA7', // Желтый
+        '#DDA0DD', // Фиолетовый
+        '#FFA07A'  // Лососевый
+    ];
+
+    async sayHello() {
         console.log("Привет из внешней библиотеки.");
         const date = new Date();
         console.log(date);
 
-        for(let i = 0; i < 1000; i++) {
-            console.log(i);
+        await this.#changeColor();
+    }
+
+    async #changeColor() {
+        for (let i = 0; i < this.#colors.length; i++) {
+            document.body.style.backgroundColor = this.#colors[i];
+            await new Promise(resolve => setTimeout(resolve, 300));
+
+            if(i == this.#colors.length - 1){
+                i = 0;
+            }
         }
     }
 }
